3967be4bcc352615e196acc64ab4c64911a2e4ac
Tevfik Tarık Alim <99840756+tarikalim@users.noreply.github.com>
2024-02-10 21:56:14 +0300

    Update DatabaseConnection.java

--- dbms_termproject/src/DatabaseConnection.java
+++ dbms_termproject/src/DatabaseConnection.java
@@ -3,8 +3,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://34.141.227.97:3306/hepsiburada_db";
-    private static final String USER = "tarik";
+    private static final String URL = "";
+    private static final String USER = "";
     private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
